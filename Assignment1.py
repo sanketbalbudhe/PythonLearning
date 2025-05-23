a6246c5a1ff4341b7b9dad5f23e392212429ce77
sanketbalbudhe <sanketbalbudhe@gmail.com>
2025-05-23 21:55:50 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,17 @@
+#Task 1: Perform Basic Mathematical Operations
+
+a = input('Enter the first number: ')
+b = input('Enter the second number: ')
+a=int(a)
+b=int(b)
+print('Addition:', a+b)
+print('Subtraction:',a-b)
+print('Multiplication:',a*b)
+print('Division:',a/b)
+
+
+#Task 2: Create a Personalized Greeting
+first =input('Enter your first name: ')
+last = input('Enter your last name: ')
+
+print('Hello,',first +' '+ last +'! Welcome to the Python program.')
